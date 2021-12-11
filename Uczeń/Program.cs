27b02b0f655a59b96febd85e3dee6c4087972d75
Risorak <artursabatowski@gmail.com>
2021-12-11 16:02:42 +0100

    Dodaj pliki projektów.

---
+++ Uczeń/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uczeń
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Uczeń A = new Uczeń(4, 5, "Kowalski");
+                Console.WriteLine(A);
+                double wynik = A.średnia();
+                Console.WriteLine(wynik);
+            } while (Console.ReadLine() = "tak");
+        }
+    }
+}
---
+++ Uczeń/Uczeń.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uczeń
+{
+    class Uczeń
+    {
+        private double ocena1, ocena2;
+        private string nazwisko;
+
+       public Uczeń(double ocena1, double ocena2, string nazwisko)
+        {
+            this.ocena1 = ocena1;
+            this.ocena2 = ocena2;
+            this.nazwisko = nazwisko;
+        }
+
+        public double średnia()
+        {
+            return (ocena1 + ocena2) / 2;
+        }
+
+        public override string ToString()
+        {
+            return $"Nazwisko ({nazwisko}) ocena 1: ({ocena1}) ocena2: ({ocena2})";
+        }
+
+    }
+}
